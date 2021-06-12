a9e6794fc2c8e2f728c3ef896bbb4435111eec28
rafi712 <rafiakmal712@gmail.com>
2021-06-12 21:07:35 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,38 @@
+/* eslint-disable import/extensions */
+import {
+  addNoteHandler,
+  getAllNotesHandler,
+  editNoteByIdHandler,
+  getNoteByIdHandler,
+  deleteNoteByIdHandler,
+} from './handlers.js';
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+export default routes;
